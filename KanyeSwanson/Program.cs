4f04959ce2d3ecb291157a2dc05ed8691d1816d5
Seth Bowman <brandonsethbowman@gmail.com>
2021-05-04 13:58:22 -0500

    project complete

--- KanyeSwanson/Program.cs
+++ KanyeSwanson/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
 
 namespace KanyeSwanson
 {
@@ -6,7 +8,40 @@ namespace KanyeSwanson
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = InitializeHttpClient();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(GetKanyeQuote(client));
+                Console.WriteLine(GetSwansonQuote(client));            
+            }
+
+            
+
+
+        }
+
+        private static string GetSwansonQuote(HttpClient client)
+        {
+            var jtext = client
+                .GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result;
+
+            var quote = JArray.Parse(jtext).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return quote;
+        }
+
+        private static string GetKanyeQuote(HttpClient client)
+        {
+            var jtext = client.GetStringAsync("https://api.kanye.rest/").Result;
+
+            var quote = JObject.Parse(jtext).GetValue("quote").ToString();
+            return quote;
+        }
+
+        private static HttpClient InitializeHttpClient()
+        {
+            return new HttpClient();
         }
     }
 }
